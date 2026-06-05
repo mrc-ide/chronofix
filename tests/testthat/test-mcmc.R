f9673de53216d4e8a88b370140e33da8bd633eb2
rebeccanash <r.nash@imperial.ac.uk>
2026-06-05 17:18:44 +0100

    update argument names in test file

--- tests/testthat/test-mcmc.R
+++ tests/testthat/test-mcmc.R
@@ -15,7 +15,7 @@ test_that("Can run mcmc", {
 test_that("Can run mcmc with cascade sampling", {
   set.seed(1)
   control <- chronofix_mcmc_control(n_steps = 50, n_chains = 3,
-                          cascade_sampling = TRUE)
+                                    cascade_sampling = TRUE)
   
   model <- toy_model(control = control)$model
   initial <- chronofix_mcmc_initial(model)
@@ -29,14 +29,14 @@ test_that("Can run mcmc with cascade sampling", {
 
 test_that("Can run mcmc with single group and single delay", {
   set.seed(1)
-  control <- mcmc_control(n_steps = 50, n_chains = 3,
-                          cascade_sampling = TRUE)
+  control <- chronofix_mcmc_control(n_steps = 50, n_chains = 3,
+                                    cascade_sampling = TRUE)
   
   model <- toy_model(control = control, single_group = "community-alive")$model
-  initial <- mcmc_initial(model)
+  initial <- chronofix_mcmc_initial(model)
   
   sampler <- chronofix_sampler(control)
-  samples <- mcmc_run(model, sampler, initial, control)
+  samples <- chronofix_mcmc_run(model, sampler, initial, control)
   
   expect_equal(dim(samples$pars), c(length(model$parameters), 50, 3))
 })
