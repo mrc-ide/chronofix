0095b58d4d7d340889d3baca831687897f3b280a
edknock <e.knock@imperial.ac.uk>
2026-08-07 14:31:25 +0100

    Fix initialisation

--- R/mcmc-initialise.R
+++ R/mcmc-initialise.R
@@ -64,8 +64,8 @@ initialise_row <- function(individual_data, group, group_delay_map,
     from_event <- valid_paths$from[i]
     to_event <- valid_paths$to[i]
 
-    date1 <- individual_data[from_event]
-    date2 <- individual_data[to_event]
+    date1 <- individual_data[as.integer(from_event)]
+    date2 <- individual_data[as.integer(to_event)]
 
     if (!is.na(date1) && !is.na(date2)) {
       # For the current path, find the direct (1-step) delays that compose it
@@ -75,8 +75,8 @@ initialise_row <- function(individual_data, group, group_delay_map,
 
       if (length(path_nodes_list) > 0) {
         path_nodes <- names(path_nodes_list[[1]])
-        direct_rules_on_path <- data.frame(from = head(path_nodes, -1),
-                                           to = tail(path_nodes, -1))
+        direct_rules_on_path <- data.frame(from = as.integer(head(path_nodes, -1)),
+                                           to = as.integer(tail(path_nodes, -1)))
 
         # Sum the min/max boundaries of these direct delays to get the total allowed range
         boundaries <- inner_join(direct_rules_on_path,
@@ -99,16 +99,17 @@ initialise_row <- function(individual_data, group, group_delay_map,
   if (length(incompatible_events) > 0) {
     problem_counts <- table(unlist(incompatible_events))
     max_problems <- max(problem_counts)
-    candidates_for_removal <- names(problem_counts[problem_counts == max_problems])
+    candidates_for_removal <- 
+      as.integer(names(problem_counts[problem_counts == max_problems]))
 
     date_to_remove <- if (length(candidates_for_removal) == 1) {
       candidates_for_removal
     } else {
       # remove the most outlying
-      date_values <- unlist(individual_data[1, candidates_for_removal])
+      date_values <- unlist(individual_data[candidates_for_removal])
       median_val <- median(date_values, na.rm = TRUE)
       outlier_idx <- which.max(abs(date_values - median_val))
-      candidates_for_removal[outlier_idx]
+      date_to_remove <- candidates_for_removal[outlier_idx]
     }
 
     individual_data[[date_to_remove]] <- as.Date(NA)
